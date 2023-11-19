047e51dfd5b5ed7dbed12a10cda95678a6d6f30c
Rishi2929 <128122653+Rishi2929@users.noreply.github.com>
2023-11-19 18:12:42 +0530

    completed todo app

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
 import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { server } from '../main'
+import React, { useContext, useEffect, useState } from 'react'
+import { Context, server } from '../main'
 import toast from 'react-hot-toast'
+import TodoItem from '../components/TodoItem'
+import { Navigate } from 'react-router-dom'
 
 function Home() {
 
@@ -9,8 +11,42 @@ function Home() {
     const [description, setDescription] = useState("")
     const [loading, setLoading] = useState(false)
     const [tasks, setTasks] = useState([])
+    const [refresh, setRefresh] = useState(false)
+    const { isAuthenticated } = useContext(Context);
 
 
+    //Update Handler
+    const updateHandler = async (id) => {
+        try {
+            const { data } = await axios.put(`${server}/task/${id}`, {},
+                {
+                    withCredentials: true
+                })
+            toast.success(data.message)
+            setRefresh(prev => !prev)
+
+        } catch (error) {
+            toast.error(error.response.data.message)
+
+        }
+    }
+
+    //Delete Handler
+    const deleteHandler = async (id) => {
+        try {
+            const { data } = await axios.delete(`${server}/task/${id}`, {
+                withCredentials: true
+            })
+            toast.success(data.message)
+            setRefresh(prev => !prev)
+
+        } catch (error) {
+            toast.error(error.response.data.message)
+        }
+    }
+
+
+    //Submit Button
     const submitHandler = async (e) => {
         e.preventDefault()
         try {
@@ -27,6 +63,7 @@ function Home() {
             setDescription("")
             toast.success(data.message)
             setLoading(false)
+            setRefresh(prev => !prev)
 
         } catch (error) {
             toast.error(error.response.data.message)
@@ -34,6 +71,7 @@ function Home() {
         }
     }
 
+    //Fetching API
     useEffect(() => {
         axios.get(`${server}/task/my`, {
             withCredentials: true
@@ -42,7 +80,10 @@ function Home() {
         }).catch(e => {
             toast.error(e.response.data.message)
         })
-    }, [])
+    }, [refresh])
+
+
+    if (!isAuthenticated) return <Navigate to={"/login"} />
 
     return (
         <div className='container'>
@@ -58,7 +99,15 @@ function Home() {
             <section className='todosContainer'>
                 {
                     tasks.map(i => (
-                        <div key={i._id}>{i.title}</div>
+                        <TodoItem
+                            title={i.title}
+                            description={i.description}
+                            isCompleted={i.isCompleted}
+                            updateHandler={updateHandler}
+                            deleteHandler={deleteHandler}
+                            id={i._id}
+                            key={i._id}
+                        />
                     ))
                 }
             </section>
